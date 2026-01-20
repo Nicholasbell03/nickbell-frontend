d33a9ec02cbf93025222c0be74cb850d0cabf580
Nicholas Bell <nick@advantagelearn.com>
2026-01-20 16:38:37 +0200

    Update routes to use App component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,20 @@
-import { lazy, Suspense } from 'react'
-import { Routes, Route } from 'react-router-dom'
-const Hero = lazy(() => import('./components/hero'))
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+const App = lazy(() => import("./App"));
 
 function AppRoutes() {
-  return (
-    <Routes>
-        <Route 
-            path="/" 
-            element={
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Hero />
-                </Suspense>
-            } 
-        />
-    </Routes>
-  )
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<Suspense fallback={<div>Loading...</div>}>
+						<App />
+					</Suspense>
+				}
+			/>
+		</Routes>
+	);
 }
 
-export default AppRoutes
+export default AppRoutes;
